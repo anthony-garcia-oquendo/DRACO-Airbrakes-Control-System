ab6f7d4a17d03d2bd040356e8e68dbf907872b32
anthony-garcia-oquendo <anthony.garcia3@upr.edu>
2026-01-29 20:39:43 -0400

    servo module

--- src/devices/servo.py
+++ src/devices/servo.py
@@ -1,3 +1,30 @@
-import logging 
+import logging
+import board 
 import pwmio
-from adafruit_motor import servo
+
+class ServoMotor:
+    ON = 2 ** 16
+    MOTOR_MIN = 0.5
+    MOTOR_MAX = 2.5
+
+    def __init__(self, pin, **kwargs):
+        self.motor = pwmio.PWMOut(pin, variable_frequency=False, **kwargs)
+        self.motor.frequency = 50
+        self.last_angle = 0
+
+    def rotate(self, n):
+       
+        try:
+            n = float(n)
+        except ValueError:
+            logging.warning(f"SERVO ERROR: must pass a number, not {n} of type {type(n)}.")
+            return
+        
+        if n < 0 or n > 45:
+            logging.warning(f"SERVO ERROR: unsafe actuation angle ({n}), stay in [0, 45].")
+            return
+
+        logging.debug(f"Actuating servo motor to {n}% = {n} degrees.")
+        duty = (n + 45)/1800
+        self.motor.duty_cycle = int(duty * ServoMotor.ON)
+        self.last_angle = n
