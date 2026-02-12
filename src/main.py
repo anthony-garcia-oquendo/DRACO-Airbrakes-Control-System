8aa7cf38e744b98d8c7616f0e4d3baf7fbc68db0
anthony-garcia-oquendo <anthony.garcia3@upr.edu>
2026-02-12 23:38:26 +0000

    servo driver test1 successful

--- src/main.py
+++ src/main.py
@@ -11,8 +11,10 @@ import logging
 import time
 import os
 
+from controller import State
+from filter import DataFilter
 from devices.sensors import BMP390, ICM20948
-from devices.servo import ServoMotor
+from devices.servo2 import ServoMotor
 
 # Output data constants.
 current_directory = os.getcwd()
@@ -62,11 +64,11 @@ writer.writerow(HEADERS)
 logging.debug(f"Output data file is open @ {OUTPUT_DATA_PATH}.")
 
 # Make the data filter.
-# logging.debug("Initializing the data filter.")
-# data_filter = DataFilter()
-# for _ in range(100):
-#     logging.debug("Processing data batch")
-#     data_filter.process_batch()
+logging.debug("Initializing the data filter.")
+data_filter = DataFilter()
+for _ in range(100):
+    logging.debug("Processing data batch")
+    # data_filter.filter_data()
 
 # Create the devices.
 logging.debug("Creating the device drivers.")
@@ -80,136 +82,136 @@ logging.debug("The device drivers are up and running.")
 logging.debug("Zeroing the altimeter.")
 try:
     # altimeter.zero()
+    servo.test_rotation(5, 0.5)
 except Exception as e:
     logging.exception("The altimeter failed to zero. This is fatal.")
     exit(1)
 logging.debug(f"The altimeter is zeroed. Reading @ {altimeter.altitude()} feet.")
 
-# Run the actuation sequence.
-logging.debug("Beginning the initial actuation sequence.")
-servo.rotate(0)
-time.sleep(2)
-servo.rotate(45)
-time.sleep(2)
-servo.rotate(0)
-logging.debug("Initial actuation sequence complete.")
+# # Run the actuation sequence.
+# logging.debug("Beginning the initial actuation sequence.")
+# servo.rotate(0)
+# time.sleep(2)
+# servo.rotate(45)
+# time.sleep(2)
+# servo.rotate(0)
+# logging.debug("Initial actuation sequence complete.")
 
-logging.debug("Initializing the actuator.")
-# actuator = ActuationController()
-logging.debug("The actuator is initialized.")
+# logging.debug("Initializing the actuator.")
+# # actuator = ActuationController()
+# logging.debug("The actuator is initialized.")
 
-logging.debug("The launch vehicle is currently on the ground.")
-state = State.PRELAUNCH
+# logging.debug("The launch vehicle is currently on the ground.")
+# state = State.PRELAUNCH
 
-start = time.time()
-logging.debug(f"Start time (Unix epoch): {start}.")
+# start = time.time()
+# logging.debug(f"Start time (Unix epoch): {start}.")
 
-logging.debug("Beginning the ACS control loop.")
-while True:
-    try:
-        # Read current time.
-        time_current = time.time() - start
+# logging.debug("Beginning the ACS control loop.")
+# while True:
+#     try:
+#         # Read current time.
+#         time_current = time.time() - start
 
-         # Read BMP390 sensor.
-        try:
-            altitude = altimeter.altitude()
-            temperature = altimeter.temperature()
-            pressure = altimeter.pressure()
-            air_density = altimeter.air_density()
-        except Exception as e:
-            logging.exception(f"Error reading the BMP390 altimeter: {e}.")
-            logging.info(f"BMP390 last altitude: {altitude}.")
-            logging.info(f"BMP390 last temperature: {temperature}.")
-            logging.info(f"BMP390 last pressure: {pressure}.")
-            logging.info(f"BMP390 last air density: {air_density}.")
-            continue
+#          # Read BMP390 sensor.
+#         try:
+#             altitude = altimeter.altitude()
+#             temperature = altimeter.temperature()
+#             pressure = altimeter.pressure()
+#             air_density = altimeter.air_density()
+#         except Exception as e:
+#             logging.exception(f"Error reading the BMP390 altimeter: {e}.")
+#             logging.info(f"BMP390 last altitude: {altitude}.")
+#             logging.info(f"BMP390 last temperature: {temperature}.")
+#             logging.info(f"BMP390 last pressure: {pressure}.")
+#             logging.info(f"BMP390 last air density: {air_density}.")
+#             continue
 
-        # Read ICM20948 sensor.
-        try:
-            acceleration = imu.acceleration()
-            acceleration_x = acceleration[0]
-            acceleration_y = acceleration[1]
-            acceleration_z = acceleration[2] - 32.15
-            magnetic = imu.magnetic()
-            magnetic_x = magnetic[0]
-            magnetic_y = magnetic[1]
-            magnetic_z = magnetic[2]
-            gyro = imu.gyro()
-            gyro_x = gyro[0]
-            gyro_y = gyro[1]
-            gyro_z = gyro[2]
-        except Exception as e:
-            logging.exception(f"Error reading the BNO055 inertial measurement unit: {e}.")
-            logging.info(f"BNO055 last acceleration: ({acceleration_x}, {acceleration_y}, {acceleration_z}).")
-            logging.info(f"BNO055 last magnetic: ({magnetic_x}, {magnetic_y}, {magnetic_z}).")
-            logging.info(f"BNO055 last gyro: ({gyro_x}, {gyro_y}, {gyro_z}).")
-            continue
+#         # Read ICM20948 sensor.
+#         try:
+#             acceleration = imu.acceleration()
+#             acceleration_x = acceleration[0]
+#             acceleration_y = acceleration[1]
+#             acceleration_z = acceleration[2] - 32.15
+#             magnetic = imu.magnetic()
+#             magnetic_x = magnetic[0]
+#             magnetic_y = magnetic[1]
+#             magnetic_z = magnetic[2]
+#             gyro = imu.gyro()
+#             gyro_x = gyro[0]
+#             gyro_y = gyro[1]
+#             gyro_z = gyro[2]
+#         except Exception as e:
+#             logging.exception(f"Error reading the BNO055 inertial measurement unit: {e}.")
+#             logging.info(f"BNO055 last acceleration: ({acceleration_x}, {acceleration_y}, {acceleration_z}).")
+#             logging.info(f"BNO055 last magnetic: ({magnetic_x}, {magnetic_y}, {magnetic_z}).")
+#             logging.info(f"BNO055 last gyro: ({gyro_x}, {gyro_y}, {gyro_z}).")
+#             continue
 
-        # Filter the data.
-        try:
-            data_filter.filter_data(altitude, acceleration_z)
-            altitude_filtered = data_filter.kalman_altitude
-            velocity_filtered = data_filter.kalman_velocity
-            acceleration_filtered = data_filter.kalman_acceleration
-        except Exception as e:
-            logging.exception(f"Error filtering the data: {e}.")
-            logging.info(f"Filter last altitude: {altitude_filtered}.")
-            logging.info(f"Filter last acceleration: {acceleration_filtered}.")
-            logging.info(f"Filter last velocity: {velocity_filtered}.")
-            continue
+#         # Filter the data.
+#         try:
+#             data_filter.filter_data(altitude, acceleration_z)
+#             altitude_filtered = data_filter.kalman_altitude
+#             velocity_filtered = data_filter.kalman_velocity
+#             acceleration_filtered = data_filter.kalman_acceleration
+#         except Exception as e:
+#             logging.exception(f"Error filtering the data: {e}.")
+#             logging.info(f"Filter last altitude: {altitude_filtered}.")
+#             logging.info(f"Filter last acceleration: {acceleration_filtered}.")
+#             logging.info(f"Filter last velocity: {velocity_filtered}.")
+#             continue
 
 
-        # Determine the state of the ACS.
-        try:
-            state = determine_state(state, altitude_filtered, velocity_filtered, acceleration_filtered)
-        except Exception as e:
-            logging.exception(f"Error within state determination: {e}.")
-            logging.info(f"Determinator last state: {state}.")
-            continue
+#         # Determine the state of the ACS.
+#         try:
+#             state = State.PRELAUNCH#determine_state(state, altitude_filtered, velocity_filtered, acceleration_filtered)
+#         except Exception as e:
+#             logging.exception(f"Error within state determination: {e}.")
+#             logging.info(f"Determinator last state: {state}.")
+#             continue
 
-        # Determine the current flap angle.
-        flap_angle = servo.last_flap_angle
+#         # Determine the current flap angle.
+#         flap_angle = servo.last_flap_angle
 
-        # Log the data
-        writer.writerow([
-            time_current,
-            state,
-            flap_angle,
-            actuator.apogee_prediction,
-            altitude_filtered,
-            velocity_filtered,
-            acceleration_filtered,
-            altitude,
-            acceleration_x,
-            acceleration_y,
-            acceleration_z,
-            magnetic_x,
-            magnetic_y,
-            magnetic_z,
-            gyro_x,
-            gyro_y,
-            gyro_z,
-            temperature,
-            pressure,
-            air_density
-        ])
+#         # Log the data
+#         writer.writerow([
+#             time_current,
+#             state,
+#             flap_angle,
+#             altitude_filtered,
+#             velocity_filtered,
+#             acceleration_filtered,
+#             altitude,
+#             acceleration_x,
+#             acceleration_y,
+#             acceleration_z,
+#             magnetic_x,
+#             magnetic_y,
+#             magnetic_z,
+#             gyro_x,
+#             gyro_y,
+#             gyro_z,
+#             temperature,
+#             pressure,
+#             air_density
+#         ])
 
-        # # Run actuation control algorithm.
-        # try:
-        #     actuation_degree = actuator.calculate_actuation(
-        #         time_current,
-        #         state,
-        #         flap_angle,
-        #         altitude_filtered,
-        #         velocity_filtered
-        #     )
-        #     if actuation_degree is not None:
-        #         flap_angle_new = servo_percentage_to_flap_angle(actuation_degree)
-        #         servo.rotate(flap_angle_new)
-        # except Exception as e:
-        #     logging.exception(f"Error within actuation control: {e}.")
-    except KeyboardInterrupt:
-        logging.warning("Keyboard interrupt detected; exiting gracefully.")
-        break
-    except BaseException as e:
-        logging.error(f"BaseException caught (this is bad): {e}")
+#         # # Run actuation control algorithm.
+#         # try:
+#         #     actuation_degree = actuator.calculate_actuation(
+#         #         time_current,
+#         #         state,
+#         #         flap_angle,
+#         #         altitude_filtered,
+#         #         velocity_filtered
+#         #     )
+#         #     if actuation_degree is not None:
+#         #         flap_angle_new = servo_percentage_to_flap_angle(actuation_degree)
+#         #         servo.rotate(flap_angle_new)
+#         # except Exception as e:
+#         #     logging.exception(f"Error within actuation control: {e}.")
+#     except KeyboardInterrupt:
+#         logging.warning("Keyboard interrupt detected; exiting gracefully.")
+#         break
+#     except BaseException as e:
+#         logging.error(f"BaseException caught (this is bad): {e}")
