ed51839e0aa0cbd89c602977256625fb38387859
anthony-garcia-oquendo <anthony.garcia3@upr.edu>
2026-02-19 23:58:04 +0000

    Delete outdated log files and flap-to-servo mapping CSV; refactor servo control in script.py; add setup documentation; update main.py for improved logging and device initialization.

--- src/scripts/manual_servo_angle.py
+++ src/scripts/manual_servo_angle.py
@@ -1,19 +1,15 @@
-from gpiozero import AngularServo
+from servo2 import ServoMotor
+import board
 from time import sleep
 
 # Initialize the servo on GPIO pin 14
 # min_pulse_width and max_pulse_width may need to be adjusted for your servo
-servo = AngularServo(14, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
-
-# Function to set the servo angle
-def set_angle(angle):
-    servo.angle = angle
-    sleep(1)
+servo = ServoMotor(board.D18)
 
 # Main program loop
 try:
     while True:
-        angle = int(input("Enter angle (0 to 180): "))  # User input for angle
-        set_angle(angle)  # Set servo to entered angle
+        angle = int(input("Enter flap angle (0 to 45): "))  # User input for angle
+        servo.rotate(angle)  # Set servo to entered angle
 except KeyboardInterrupt:
     print("Program stopped by user")
